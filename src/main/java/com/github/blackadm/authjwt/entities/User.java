55b57d9950e8514cbabbace751eca507d60b34de
black-adm <mattheuscontato17@gmail.com>
2023-11-09 21:56:59 -0300

    created user entity

---
+++ src/main/java/com/github/blackadm/authjwt/entities/User.java
@@ -0,0 +1,29 @@
+package com.github.blackadm.authjwt.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue
+    private UUID id;
+    
+    private String name;
+    
+    private String email;
+
+    private String password;
+}
