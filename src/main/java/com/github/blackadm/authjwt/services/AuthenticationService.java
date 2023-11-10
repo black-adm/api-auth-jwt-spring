e81a6c41318fa7a573446c3bb56b427cae2e03ad
black-adm <mattheuscontato17@gmail.com>
2023-11-10 05:33:08 -0300

    update User entity and implementing Authentication Service logic

--- src/main/java/com/github/blackadm/authjwt/services/AuthenticationService.java
+++ src/main/java/com/github/blackadm/authjwt/services/AuthenticationService.java
@@ -1,5 +1,26 @@
 package com.github.blackadm.authjwt.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import com.github.blackadm.authjwt.entities.User;
+
+@Service
 public class AuthenticationService {
-    
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    TokenService tokenService;
+
+    public String findToken(User user) {
+        var userData = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        var auth = this.authenticationManager.authenticate(userData);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return token;
+    }
 }
