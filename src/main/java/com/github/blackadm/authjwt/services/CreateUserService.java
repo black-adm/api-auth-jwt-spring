a8a58d4a1d68eab1d62532a5ff7420ec46c443f3
black-adm <mattheuscontato17@gmail.com>
2023-11-09 22:05:11 -0300

    user service implemented and verify email already exists on system

--- src/main/java/com/github/blackadm/authjwt/services/CreateUserService.java
+++ src/main/java/com/github/blackadm/authjwt/services/CreateUserService.java
@@ -1,5 +1,24 @@
 package com.github.blackadm.authjwt.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.blackadm.authjwt.entities.User;
+import com.github.blackadm.authjwt.repositories.UserRepository;
+
+@Service
 public class CreateUserService {
     
+    @Autowired
+    UserRepository userRepository;
+
+    public User execute(User user) {
+        User existUser = userRepository.findByEmail(user.getEmail());
+
+        if (existUser == null) {
+            throw new Error("Este email já possui cadastro no sistema");
+        }
+
+        return userRepository.save(user);
+    }
 }
