6e90d5d2e75d558d919c4846bb7774fb9e9a73e8
black-adm <mattheuscontato17@gmail.com>
2023-11-09 21:50:59 -0300

    project base

---
+++ src/test/java/com/github/blackadm/authjwt/AuthjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.blackadm.authjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
