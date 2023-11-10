ce8b6a898c87d92216a7079d749576c0d29cc0fe
black-adm <mattheuscontato17@gmail.com>
2023-11-10 05:35:15 -0300

    add login user route

--- src/main/java/com/github/blackadm/authjwt/controllers/AuthController.java
+++ src/main/java/com/github/blackadm/authjwt/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.github.blackadm.authjwt.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,23 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.blackadm.authjwt.entities.User;
 import com.github.blackadm.authjwt.repositories.UserRepository;
+import com.github.blackadm.authjwt.services.AuthenticationService;
 import com.github.blackadm.authjwt.services.CreateUserService;
+import com.github.blackadm.authjwt.services.TokenService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    
+
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     CreateUserService createUserService;
 
-    @PostMapping
-    public User create(@RequestBody @Valid User user) {
-        return createUserService.execute(user);
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @Autowired
+    TokenService tokenService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> create(@RequestBody @Valid User user) {
+        createUserService.execute(user);
+        return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid User user) {
+            String token = authenticationService.findToken(user);
+            return ResponseEntity.ok(token);
+    }
 }
